871f958e6dd80bbd65f651ac18aae3ebb0de2025
Sukron <sukron.213040095@mail.unpas.ac.id>
2022-11-19 16:00:06 +0700

    Toast Modul

---
+++ here.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class ToastMessage extends Component {
+    render() { 
+        return ( <div id='toasthere'></div> );
+    }
+}
+ 
+export default ToastMessage;
